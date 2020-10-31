892616ed3febd2bf1477ca880413b3bf305eea1c
Lucas <luoz@appstate.edu>
2020-10-31 15:42:14 -0400

    Initial Commit

---
+++ CrimeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CrimeApp
+//
+//  Created by lucas on 10/31/20.
+//  Copyright © 2020 lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
